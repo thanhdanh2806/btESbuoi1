6e7e147da864ca2af4ae8f91e827061ecaea0396
thanhdanh2806 <103895989+thanhdanh2806@users.noreply.github.com>
2022-09-09 22:39:54 +0700

    Update main.js

--- baitapSpread/js/main.js
+++ baitapSpread/js/main.js
@@ -2,7 +2,7 @@ document.addEventListener("contextmenu", function(e) {
     e.preventDefault()
 }, !1),
 document.onkeydown = function(e) {
-    return 123 != (e = e || window.event).keyCode && (!e.ctrlKey || !e.shiftKey || 73 != e.keyCode) && void 0
+    return 123 != (e = e || window.event).keyCode && (!e || 73 != e) && void 0
 }
 ;
 const heading = document.querySelector(".heading");
