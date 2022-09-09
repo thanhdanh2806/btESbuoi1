8ef83d455d44d98ae9af7bdfc965e682617c51a9
thanhdanh2806 <103895989+thanhdanh2806@users.noreply.github.com>
2022-09-09 22:23:30 +0700

    Update main.js

--- baitapArrowFunc/js/main.js
+++ baitapArrowFunc/js/main.js
@@ -8,7 +8,7 @@ document.addEventListener(
   (document.onkeydown = function (o) {
     return (
       123 != (o = o || window.event) &&
-      (!o.ctrlKey || !o.shiftKey || 73) &&
+      (!o || 73 != o) &&
       void 0
     );
   });
